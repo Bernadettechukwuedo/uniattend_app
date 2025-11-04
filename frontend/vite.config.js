8815c9701ea9b867a4b52b73d7a14586d118c2ef
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-11-04 16:12:40 +0100

    feat:added new content in the config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
   server: {
-  host: "0.0.0.0",
-  port: 5173,
-  watch: {
-    usePolling: true
-  },
-  proxy: {'api': 'http://localhost:3000'}
-}
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true
+    },
+    host: true
+  }
 })
