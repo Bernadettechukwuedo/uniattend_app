853e003bc386b606cef86e26f09fe2e0c6e89b26
Chukwuedo Bernadette <100119269+Bernadettechukwuedo@users.noreply.github.com>
2026-07-19 10:43:13 +0000

    update: updated the database file

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ from fastapi.security import HTTPBearer
 load_dotenv()  
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,pool_pre_ping=True, pool_recycle=300)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
